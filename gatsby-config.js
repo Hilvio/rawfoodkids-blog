3ad729303ac367416bc0834329a26f1db49ed00b
Jonne Hirvi <jonnehir@gmail.com>
2020-02-23 20:44:45 +0200

    Fix manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,25 +16,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
-        start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
-      },
-    },
-    'gatsby-plugin-offline',
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
@@ -59,15 +40,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Raw Food Kids`,
+        short_name: `RFK`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: "standalone",
         icon: `content/assets/icon.png`,
+        crossOrigin: `use-credentials`,
       },
     },
+    'gatsby-plugin-offline',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
